d2c5d844a4e0754ef0005a2359f101b739961ac3
andreiadourado <106832026+andreiadourado@users.noreply.github.com>
2023-05-08 23:39:24 -0300

    Add files via upload

---
+++ TestaVariaveis.java
@@ -0,0 +1,13 @@
+package src;
+
+public class TestaVariaveis{
+
+    public static void main(String [] args){
+
+        System.out.println("ola novo teste");    
+
+        int idade = 37;
+
+        System.out.println("a idade é "+ idade + ", parabéns!");
+    }
+}
